17b5785c70c421d6eeb09c07a792744e791c5b85
cod3prod <cod3prod@gmail.com>
2024-12-21 18:47:51 +0900

    🚀 Deploy server

--- server/src/config/database.config.ts
+++ server/src/config/database.config.ts
@@ -1,11 +1,12 @@
 import { registerAs } from "@nestjs/config";
 
 export default registerAs('database', () => ({
-    host: process.env.DATABASE_HOST || 'localhost',
-    port: process.env.DATABASE_PORT || 5432,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
+    // host: process.env.DATABASE_HOST || 'localhost',
+    // port: process.env.DATABASE_PORT || 5432,
+    // username: process.env.DATABASE_USERNAME,
+    // password: process.env.DATABASE_PASSWORD,
+    // database: process.env.DATABASE_NAME,
+    url: process.env.DATABASE_URL,
     synchronize: process.env.DATABASE_SYNCHRONIZE==='true'? true: false,
     autoLoadEntities: process.env.DATABASE_AUTOLOAD==='true'? true: false,
 }))
